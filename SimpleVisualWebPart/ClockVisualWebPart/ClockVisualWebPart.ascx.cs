fb4acb591ed5e06f805182087323d848c5ff5360
Frederik Leksell <Frederik@Frederiks-MacBook-Pro.local>
2013-12-25 15:55:51 +0100

    Version 1.0 Upload

---
+++ SimpleVisualWebPart/ClockVisualWebPart/ClockVisualWebPart.ascx.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.ComponentModel;
+using System.Web.UI.WebControls.WebParts;
+
+namespace SimpleVisualWebPart.ClockVisualWebPart
+{
+    [ToolboxItemAttribute(false)]
+    public partial class ClockVisualWebPart : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public ClockVisualWebPart()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+// If you have another name on your project and web part, then copy only the things under here and replace what you have under the base.OnInit above here.
+
+        //Countdown settings
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        private string _ClockBackgroundColor;
+
+        [Personalizable(PersonalizationScope.Shared), WebBrowsable(true),
+        WebDisplayName("Clock background color should use HEX (#999999)"),
+        WebDescription("Choose your background color (#999999)")]
+        public string ClockBackgroundColor
+        {
+            get
+            {
+                return _ClockBackgroundColor;
+            }
+            set
+            {
+                _ClockBackgroundColor = value;
+            }
+        }
+
+        //Countdown settings
+
+        private string _ClockTextColor;
+
+        [Personalizable(PersonalizationScope.Shared), WebBrowsable(true),
+        WebDisplayName("Clock text color should use HEX (#ffffff)"),
+        WebDescription("Choose your text color (#ffffff)")]
+        public string ClockTextColor
+        {
+            get
+            {
+                return _ClockTextColor;
+            }
+            set
+            {
+                _ClockTextColor = value;
+            }
+        }
+    }
+}
+
+
---
+++ SimpleVisualWebPart/Features/Activate Clock Web Part/Activate Clock Web Part.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="8fb28a06-a4ca-431f-a2ac-ff2b68b8fbc9" featureId="8fb28a06-a4ca-431f-a2ac-ff2b68b8fbc9" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="Activate Clock Web Part" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="2a207b49-24da-4ebc-83ac-da835865ce5a" />
+  </projectItems>
+</feature>
